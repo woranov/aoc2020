851fe1d8c279d77aa624644a2e2aab1ec40a8a5a
woranov <wor4nov@gmail.com>
2020-12-05 15:10:41 +0100

    day 4 part 1 haskell

--- README.md
+++ README.md
@@ -32,3 +32,10 @@ nim c --hints:off -o:./dayXX/ -r ./dayXX/part1.nim
 nim c --hints:off -o:./dayXX/ -r ./dayXX/part2.nim
 rm ./dayXX/*.exe
 ```
+
+## Haskell
+```console
+ghc -v0 ./dayXX/part1.hs && ./dayXX/part1
+ghc -v0 ./dayXX/part2.hs && ./dayXX/part2
+rm ./dayXX/* -Include *.exe,*.hi,*.o
+```
---
+++ day04/part1.hs
@@ -0,0 +1,61 @@
+import           Data.Function
+import           Data.List
+import           System.Environment
+import           System.FilePath
+
+
+-- implemented most of the stuff before i found out about haskell's "standard library"
+
+
+type Block = [String]
+type Entry = (String, String)
+type Passport = [Entry]
+
+
+required :: [String]
+required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+
+
+split :: Eq a => a -> [a] -> [[a]]
+split d s = case span (/= d) s of
+  (x, _ : ts) -> x : split d ts
+  (x, []    ) -> x : []
+
+
+-- https://stackoverflow.com/a/23979003
+sameElems :: Eq a => [a] -> [a] -> Bool
+sameElems xs ys = null (xs \\ ys) && null (ys \\ xs)
+
+
+blocks :: [String] -> [Block]
+blocks [] = []
+blocks ls = a' : blocks b
+ where
+  (a, b) = case span (/= "") ls of
+    ([], b) -> ([], tail b)
+    (a , b) -> (a, b)
+  a' = concatMap (split ' ') a
+
+
+entry :: String -> Entry
+entry s = case split ':' s of
+  [a, b] -> (a, b)
+
+
+passport :: Block -> Passport
+passport xs = map entry xs
+
+
+checkPassport :: Passport -> Bool
+checkPassport p = intersect (map fst p) required `sameElems` required
+
+
+compute :: [String] -> Int
+compute ls = ls & blocks & map passport & filter checkPassport & length
+
+
+main :: IO ()
+main = do
+  exePath <- getExecutablePath
+  content <- readFile (replaceFileName exePath "input.txt")
+  content & lines & compute & show & putStrLn
