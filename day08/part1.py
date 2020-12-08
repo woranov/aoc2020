0492884ff011372ec2e5364b1b6d2a82582a3938
woranov <wor4nov@gmail.com>
2020-12-08 16:03:45 +0100

    day 8

---
+++ day08/part1.py
@@ -0,0 +1,50 @@
+_TESTCASE = """\
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+""".strip().splitlines()
+
+
+def compute(data):
+    """
+    >>> compute(_TESTCASE)
+    5
+    """
+    visited = set()
+    acc = 0
+    row = 0
+
+    while row not in visited:
+        visited.add(row)
+
+        op, num_s = data[row].split()
+        num = int(num_s.lstrip("+"))
+
+        if op == "nop":
+            row += 1
+        elif op == "acc":
+            acc += num
+            row += 1
+        elif op == "jmp":
+            row += num
+
+    return acc
+
+
+def main():
+    import pathlib
+
+    input_path = pathlib.Path(__file__).with_name("input.txt")
+
+    with input_path.open() as f:
+        print(compute(f.read().splitlines()))
+
+
+if __name__ == "__main__":
+    main()
---
+++ day08/part2.py
@@ -0,0 +1,71 @@
+import functools
+
+_TESTCASE = """\
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+""".strip().splitlines()
+
+
+def compute(data):
+    """
+    >>> compute(_TESTCASE)
+    8
+    """
+
+    # noinspection PyUnboundLocalVariable
+    def execute(ops, acc, row, visited, switched=False):
+        visited = set(visited)
+
+        while row < len(data):
+            if row in visited:
+                return None
+            else:
+                visited.add(row)
+
+            op, num = ops[row]
+            switch = functools.partial(
+                execute, ops=ops, acc=acc, visited=visited, switched=True
+            )
+
+            if op == "acc":
+                acc += num
+                row += 1
+            elif op == "nop":
+                if not switched and (alternate := switch(row=row + num)):
+                    return alternate
+                else:
+                    row += 1
+            elif op == "jmp":
+                if not switched and (alternate := switch(row=row + 1)):
+                    return alternate
+                else:
+                    row += num
+
+        return acc
+
+    return execute(
+        ops=[(op, int(num_s.lstrip("+"))) for op, num_s in map(str.split, data)],
+        acc=0,
+        row=0,
+        visited=set(),
+    )
+
+
+def main():
+    import pathlib
+
+    input_path = pathlib.Path(__file__).with_name("input.txt")
+
+    with input_path.open() as f:
+        print(compute(f.read().splitlines()))
+
+
+if __name__ == "__main__":
+    main()
