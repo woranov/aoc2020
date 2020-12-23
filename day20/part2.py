ffe2fc2cb40f26790c8a2077a457bfc83df81492
woranov <wor4nov@gmail.com>
2020-12-23 01:31:44 +0100

    day 20 part 2

--- day20/part2.py
+++ day20/part2.py
@@ -1,7 +1,13 @@
 import collections
+import functools
 import pathlib
-from pprint import pprint
-from typing import NamedTuple
+from typing import List, NamedTuple
+
+MONSTER = [
+    "                  # ",
+    "#    ##    ##    ###",
+    " #  #  #  #  #  #   ",
+]
 
 
 class Edges(NamedTuple):
@@ -11,9 +17,35 @@ class Edges(NamedTuple):
     left: str
 
 
-class Tile(NamedTuple):
+class Tile:
     tile_id: int
-    edges: Edges
+    rows: List[str]
+
+    def __init__(self, tile_id, rows):
+        self.tile_id = tile_id
+        self.rows = rows
+
+    @functools.cached_property
+    def edges(self):
+        return Edges(
+            top=self.rows[0],
+            right="".join(row[-1] for row in self.rows),
+            bottom=self.rows[-1],
+            left="".join(row[0] for row in self.rows),
+        )
+
+    top = property(lambda self: self.edges.top)
+    right = property(lambda self: self.edges.right)
+    bottom = property(lambda self: self.edges.bottom)
+    left = property(lambda self: self.edges.left)
+
+    @functools.lru_cache()
+    def orientations(self):
+        return [
+            (self.flip() if flip else self).rotate90(rot)
+            for rot in range(4)
+            for flip in (False, True)
+        ]
 
     @classmethod
     def from_input(cls, input_data):
@@ -21,86 +53,48 @@ class Tile(NamedTuple):
         _, _, tile_id = input_data.pop(0).partition(" ")
         tile_id = int(tile_id.rstrip(":"))
 
-        """
-        a b c
-        d   e
-        f g h
-        """
-        edges = Edges(
-            top=input_data[0],
-            right="".join(row[-1] for row in input_data),
-            bottom=input_data[-1],
-            left="".join(row[0] for row in input_data),
-        )
-
-        return cls(tile_id=tile_id, edges=edges)
+        return cls(tile_id=tile_id, rows=input_data)
 
     def flip(self):
-        new_top = self.edges.top[::-1]
-        new_bottom = self.edges.bottom[::-1]
-        new_left = self.edges.right
-        new_right = self.edges.left
-
-        return self._replace(edges=Edges(new_top, new_right, new_bottom, new_left))
+        return type(self)(tile_id=self.tile_id, rows=[row[::-1] for row in self.rows])
 
     def rotate90(self, times=1):
-        new_top, new_right, new_bottom, new_left = self.edges
-        for _ in range(times):
-            (new_top, new_right, new_bottom, new_left) = (
-                new_left[::-1],
-                new_top,
-                new_right[::-1],
-                new_bottom,
-            )
+        rows = self.rows.copy()
 
-        return self._replace(edges=Edges(new_top, new_right, new_bottom, new_left))
+        for _ in range(times % 4):
+            rows = ["".join(fs) for fs in zip(*reversed(rows))]
+
+        return type(self)(tile_id=self.tile_id, rows=rows)
+
+    def edgeless(self):
+        return type(self)(tile_id=self.tile_id, rows=[row[1:-1] for row in self.rows[1:-1]])
 
     def __str__(self):
-        rows = []
-
-        rows.append(self.edges.top)
-
-        fill = "." * (len(self.edges.top) - 2)
-
-        for left, right in zip(self.edges.left[1:-1], self.edges.right[1:-1]):
-            rows.append(f"{left}{fill}{right}")
-
-        rows.append(self.edges.bottom)
-
-        return "\n".join(row.replace("", " ").strip() for row in rows)
+        return "\n".join(self.rows)
 
     def __repr__(self):
         return f"<{type(self).__name__} {repr(self.tile_id)}>"
 
+    def __eq__(self, other):
+        return isinstance(other, Tile) and self.tile_id == other.tile_id and self.rows == other.rows
 
-"""
-SAMPLE
-
-1951    2311    3079
-2729    1427    2473
-2971    1489    1171
-"""
+    def __hash__(self):
+        return hash((self.tile_id, *self.rows))
 
 
 def compute(data):
     """
     >>> compute(pathlib.Path(__file__).with_name("sample.txt").read_text())
-    20899048083289
+    273
     """
     tiles = {(tile := Tile.from_input(inp)).tile_id: tile for inp in data.split("\n\n")}
 
-    permutations = {
-        (tile.tile_id, r, bool(f)): (tile.flip() if f else tile).rotate90(r)
-        for tile in tiles.values()
-        for r in range(4)
-        for f in range(2)
-    }
+    edge_tiles = collections.defaultdict(lambda: collections.defaultdict(dict))
 
-    edge_tiles = collections.defaultdict(lambda: collections.defaultdict(list))
-
-    for (tid, r, f), permutation in permutations.items():
-        for edge_pos, edge in permutation.edges._asdict().items():
-            edge_tiles[edge][tid].append((r, f, edge_pos))
+    for tile in tiles.values():
+        for oriented_tile in tile.orientations():
+            for edge_pos, edge in oriented_tile.edges._asdict().items():
+                edge_tiles[edge][oriented_tile.tile_id][edge_pos] = oriented_tile
 
     tiles_by_common_edges = collections.Counter(
         tile for tiles in edge_tiles.values() for tile in tiles if len(tiles) == 2
@@ -122,21 +116,126 @@ def compute(data):
             elif common == 8:
                 s = inside_tile_edges
             else:
-                assert False
+                assert False, f"illegal number of common edges: {common}"
 
             s[tile].add(edge)
 
-    print("corner", end="  ")
-    pprint(corner_tile_edges)
-    print("border", end="  ")
-    pprint(border_tile_edges)
-    print("inside", end="  ")
-    pprint(inside_tile_edges)
+    def pop_tile(tile_set, top=None, right=None, bottom=None, left=None):
+        candidates = None
+
+        for pos, constr in dict(top=top, right=right, bottom=bottom, left=left).items():
+            constr_candidates = set()
+            if constr is None:
+                continue
+            else:
+                for tile_id, pos_map in edge_tiles[constr].items():
+                    if tile_id not in tile_set:
+                        continue
+                    if pos in pos_map:
+                        constr_candidates.add(pos_map[pos])
+
+            if candidates is None:
+                candidates = constr_candidates
+            else:
+                candidates &= constr_candidates
+
+        tile = next(iter(candidates))
+        del tile_set[tile.tile_id]
+        return tile
+
+    square_width = int(len(tiles) ** 0.5)
+
+    def first_row():
+        row = [
+            curr_tile := next(
+                tile
+                for tile_id, common_edges in corner_tile_edges.items()
+                for tile in tiles[tile_id].orientations()
+                if tile.left not in common_edges
+                if tile.top not in common_edges
+            )
+        ]
+        del corner_tile_edges[curr_tile.tile_id]
+
+        return (
+            row
+            + [
+                curr_tile := pop_tile(border_tile_edges, left=curr_tile.right)
+                for _ in range(square_width - 2)
+            ]
+            + [pop_tile(corner_tile_edges, left=curr_tile.right)]
+        )
+
+    def mid_row(row_before):
+        return (
+            [curr_tile := pop_tile(border_tile_edges, top=row_before[0].bottom)]
+            + [
+                curr_tile := pop_tile(
+                    inside_tile_edges, top=row_before[i].bottom, left=curr_tile.right
+                )
+                for i in range(1, square_width - 1)
+            ]
+            + [pop_tile(border_tile_edges, top=row_before[-1].bottom, left=curr_tile.right)]
+        )
+
+    def last_row(row_before):
+        return (
+            [curr_tile := pop_tile(corner_tile_edges, top=row_before[0].bottom)]
+            + [
+                curr_tile := pop_tile(
+                    border_tile_edges, top=row_before[i].bottom, left=curr_tile.right
+                )
+                for i in range(1, square_width - 1)
+            ]
+            + [pop_tile(corner_tile_edges, top=row_before[-1].bottom, left=curr_tile.right)]
+        )
+
+    rows = (
+        [curr_row := first_row()]
+        + [curr_row := mid_row(curr_row) for _ in range(square_width - 2)]
+        + [last_row(curr_row)]
+    )
+
+    image = [
+        "".join(sym_row)
+        for row in rows
+        for sym_row in zip(*(str(tile.edgeless()).splitlines() for tile in row))
+    ]
+
+    image_width = len(image)
+
+    for monster in Tile(666, rows=MONSTER).orientations():
+        monster_height = len(monster.rows)
+        monster_width = len(monster.rows[0])
+
+        count = sum(
+            all(
+                monster_sym == image_sym or monster_sym == " "
+                for monster_row, image_row in zip(
+                    monster.rows, image[y_idx : y_idx + monster_height]
+                )
+                for monster_sym, image_sym in zip(
+                    monster_row, image_row[x_idx : x_idx + monster_width]
+                )
+            )
+            for x_idx in range(image_width - monster_width)
+            for y_idx in range(image_width - monster_height)
+        )
+
+        if count:
+            break
+    else:
+        assert False, "no monsters found"
+
+    image_hash_count = sum(image_row.count("#") for image_row in image)
+    monster_hash_count = sum(monster_row.count("#") for monster_row in MONSTER)
+
+    return image_hash_count - count * monster_hash_count
 
 
 def main():
-    # input_path = pathlib.Path(__file__).with_name("input.txt")
-    input_path = pathlib.Path(__file__).with_name("sample.txt")
+    input_path = pathlib.Path(__file__).with_name("input.txt")
+    # input_path = pathlib.Path(__file__).with_name("sample.txt")
 
     print(compute(input_path.read_text()))
 
